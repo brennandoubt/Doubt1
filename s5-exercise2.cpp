4eba2bd4832b69418f3b75be798e8ca809b65570
brennandoubt <brennandoubt@gmail.com>
2022-05-10 01:22:16 -0400

    Drew two triangles using two different VAOs and two different VBOs with two different vertex arrays

--- s5-exercise2.cpp
+++ s5-exercise2.cpp
@@ -78,6 +78,111 @@ int main() {
 		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << finfoLog << endl;
 	}
 
+	// setting up program to attach and link shaders with
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int psuccess;
+	char pinfoLog[512];
+	glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &psuccess);
+	if (!psuccess) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, pinfoLog);
+		cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << endl;
+	}
+
+	// deleting shader objects after linking to program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Goal: Draw two triangles side-by-side using two different VBOs and VAOs for their data.
+	
+	// triangle 1
+	float vertices1[] = {
+		-0.7f, -0.8f, 0.0f,
+		-0.1f, -0.8f, 0.0f,
+		-0.4f, 0.0f, 0.0f
+	};
+	// triangle 2
+	float vertices2[] = {
+		0.7f, -0.8f, 0.0f,
+		0.1f, -0.8f, 0.0f,
+		0.4f, 0.0f, 0.0f
+	};
+
+	// A vertex array object(VAO) also keeps track of EBO bindings, so the last EBO bound while a VAO is bound, is stored as the
+	// VAO's EBO; a VAO stores the glBindBuffer calls when the buffer target is GL_ELEMENT_ARRAY_BUFFER (also stores its unbind calls)
+
+
+	// first vao
+	unsigned int VAO1;
+	glGenVertexArrays(1, &VAO1);
+
+	// first vbo
+	unsigned int VBO1;
+	glGenBuffers(1, &VBO1);
+
+	// bind vao 1
+	glBindVertexArray(VAO1);
+
+	// bind vbo 1 to array buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+	// copy triangle 1 vertices array into vbo 1
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+	// configure vbo 1 (set vertex attribute pointers)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// unbind vao 1
+	glBindVertexArray(VAO1);
+
+
+	// second vao
+	unsigned int VAO2;
+	glGenVertexArrays(1, &VAO2);
+
+	// second vbo
+	unsigned int VBO2;
+	glGenBuffers(1, &VBO2);
+
+	// bind vao 2
+	glBindVertexArray(VAO2);
+
+	// bind vbo 2 to array buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+
+	// copy triangle 2 vertices array into vbo 2
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+	// configure vbo 2 vertex attributes (tells OpenGL how to read each vertex attribute (each vertex attribute = 3 float-type graph positions x, y, and z))
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// unbind vao 2
+	glBindVertexArray(VAO2);
+
+
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		glUseProgram(shaderProgram);
+
+		// bind vao 1 to draw triangle 1
+		glBindVertexArray(VAO1);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// then bind vao 2 to draw triangle 2
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+		processInput(window);
+		glfwPollEvents();
+		glfwSwapBuffers(window); // (changes window from white to black)
+	}
 
 
 
