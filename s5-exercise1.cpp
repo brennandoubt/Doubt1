e07ed906f0b8c797624a6e0159262f2a47ae3a89
brennandoubt <brennandoubt@gmail.com>
2022-05-09 22:31:16 -0400

    Triangle outputted is now orange (but issue found was with a vertex shader function parameter)

--- s5-exercise1.cpp
+++ s5-exercise1.cpp
@@ -50,6 +50,65 @@ int main() {
 		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\0";
 
+	// creating vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// vertex shader error handling
+	int vsuccess;
+	char vinfolog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vsuccess);
+	if (!vsuccess) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, vinfolog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vinfolog << endl;
+	}
+
+	// creating fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// fragment shader error handling
+	int fsuccess;
+	char finfolog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fsuccess);
+	if (fsuccess) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, finfolog);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << finfolog << endl;
+	}
+
+
+	// now link shaders into a shader program
+
+	// creating program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram(); // also returns ID to reference program object
+
+	// attaching shaders to program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// linking shaders attached to program
+	glLinkProgram(shaderProgram);
+
+	// shader program error handling
+	int psuccess;
+	char pinfolog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &psuccess);
+	if (!psuccess) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, pinfolog);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << pinfolog << endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	// *can now delete shader objects since they can now just be referenced from program?
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	// first (left) triangles' vertices
 	float vertices[] = {
 		-0.7f, -0.8f, 0.0f, // bottom left ((-1.0f, -1.0f, 0.0f) normalized would be the very bottom left corner coordinate of render window)
@@ -63,6 +122,13 @@ int main() {
 		0.6f, 0.0f, 0.0f
 	};
 
+	// generate vertex array object to store state configurations (vertex attribute configurations?)
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	// bind VAO to use it
+	glBindVertexArray(VAO);
+
 	// generating vertex buffer (object) to hold vertex data
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
@@ -73,17 +139,24 @@ int main() {
 	// add vertex data for first triangle to buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// unbind buffer from array buffer target
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	// set and enable vertex attributes as inputs to OpenGL so it can interpret the vertices' data as intended
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
+	// set and enable vertex attributes as inputs to OpenGL so it can interpret the vertices' data as intended (only a blank window is outputted without this code)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// use shader program when we want to render an object
+	glUseProgram(shaderProgram);
 
 	/* Render Loop */
 	while (!glfwWindowShouldClose(window)) {
-		
 
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // clear window with this color generated (state-setting function)
+		glClear(GL_COLOR_BUFFER_BIT); // fill color buffer with specified color to clear it (state-using function)
+
+		// using shader program when rendering
+		//glUseProgram(shaderProgram);
+		// re-binding VAO here to use the preferred settings for triangles
+		glBindVertexArray(VAO);
+		// draws the primitives using currently active shader, vertex attributes, and vertex buffer's data (indirectly bound via vertex array object)
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		processInput(window);
